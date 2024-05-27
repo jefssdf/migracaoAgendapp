380e5455393169b964908d9eafc76d0c14bd5906
Ralf Filho <testeapp024@gmail.com>
2024-05-27 19:30:22 -0300

    Integrando com API

--- src/pages/cadastro-cliente.vue
+++ src/pages/cadastro-cliente.vue
@@ -1,124 +1,113 @@
 <template>
-    <div class="q-pa-md" style="max-width: 400px">
-      <h1 class="text-h6">Cadastro de Cliente</h1>
-  
-      <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
-        <q-input
-          filled
-          v-model="fullName"
-          label="Nome Completo *"
-          hint="Nome e Sobrenome"
-          lazy-rules
-          :rules="[ val => val && val.length > 0 || 'Por favor, digite algo']"
-        />
-  
-        <q-input
-          filled
-          type="date"
-          v-model="birthDate"
-          label="Data de Nascimento *"
-          lazy-rules
-          :rules="[ val => val !== null && val !== '' || 'Por favor, digite sua data de nascimento']"
-        />
-  
-        <q-input
-          filled
-          v-model="contact"
-          label="Contato"
-          lazy-rules
-        />
-  
-        <q-input
-          filled
-          type="email"
-          v-model="email"
-          label="Email *"
-          lazy-rules
-          :rules="[ val => /\S+@\S+\.\S+/.test(val) || 'Por favor, digite um email válido']"
-        />
-  
-        <q-input
-          filled
-          type="password"
-          v-model="password"
-          label="Senha *"
-          lazy-rules
-          :rules="[ val => val && val.length >= 8 || 'A senha deve conter pelo menos 8 caracteres']"
-        />
-  
-        <q-input
-          filled
-          type="password"
-          v-model="confirmPassword"
-          label="Confirmar Senha *"
-          lazy-rules
-          :rules="[ val => val === password || 'As senhas não coincidem']"
-        />
-  
-        <q-toggle v-model="accept" label="Eu aceito os termos e licença" />
-  
-        <div>
-          <q-btn label="Enviar" type="submit" color="primary"/>
-          <q-btn label="Limpar" type="reset" color="primary" flat class="q-ml-sm" />
-        </div>
-      </q-form>
-    </div>
-  </template>
-  
-  <script>
-  import { useQuasar } from 'quasar'
-  import { ref } from 'vue'
-  
-  export default {
-    setup () {
-      const $q = useQuasar()
-  
-      const fullName = ref(null)
-      const birthDate = ref(null)
-      const contact = ref(null)
-      const email = ref(null)
-      const password = ref(null)
-      const confirmPassword = ref(null)
-      const accept = ref(false)
-  
-      return {
-        fullName,
-        birthDate,
-        contact,
-        email,
-        password,
-        confirmPassword,
-        accept,
-  
-        onSubmit () {
-          if (accept.value !== true) {
-            $q.notify({
-              color: 'red-5',
-              textColor: 'white',
-              icon: 'warning',
-              message: 'Você precisa aceitar os termos e licença primeiro'
-            })
-          } else {
-            $q.notify({
-              color: 'green-4',
-              textColor: 'white',
-              icon: 'cloud_done',
-              message: 'Enviado'
-            })
-          }
-        },
-  
-        onReset () {
-          fullName.value = null
-          birthDate.value = null
-          contact.value = null
-          email.value = null
-          password.value = null
-          confirmPassword.value = null
-          accept.value = false
+  <div class="q-pa-md" style="max-width: 400px">
+    <h1 class="text-h6">Cadastro de Cliente</h1>
+
+    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+      <q-input
+        filled
+        v-model="fullName"
+        label="Nome Completo *"
+        hint="Nome e Sobrenome"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Por favor, digite algo']"
+      />
+
+      <q-input
+        filled
+        v-model="contact"
+        label="Contato"
+        lazy-rules
+      />
+
+      <q-input
+        filled
+        type="email"
+        v-model="email"
+        label="Email *"
+        lazy-rules
+        :rules="[ val => /\S+@\S+\.\S+/.test(val) || 'Por favor, digite um email válido']"
+      />
+
+      <q-input
+        filled
+        type="password"
+        v-model="password"
+        label="Senha *"
+        lazy-rules
+        :rules="[ val => val && val.length >= 8 || 'A senha deve conter pelo menos 8 caracteres']"
+      />
+
+      <q-input
+        filled
+        type="password"
+        v-model="confirmPassword"
+        label="Confirmar Senha *"
+        lazy-rules
+        :rules="[ val => val === password || 'As senhas não coincidem']"
+      />
+
+      <q-toggle v-model="accept" label="Eu aceito os termos e licença" />
+
+      <div>
+        <q-btn label="Enviar" type="submit" color="primary"/>
+        <q-btn label="Limpar" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+    </q-form>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import axios from 'axios'
+
+export default {
+  setup () {
+    const fullName = ref(null)
+    const contact = ref(null)
+    const email = ref(null)
+    const password = ref(null)
+    const confirmPassword = ref(null)
+    const accept = ref(false)
+
+    const onSubmit = async () => {
+      if (accept.value !== true) {
+        console.log('Você precisa aceitar os termos e licença primeiro')
+      } else {
+        try {
+          const response = await axios.post('https://65ff33d3df565f1a6144de0b.mockapi.io/lista-clientes', {
+            name: fullName.value,
+            email: email.value,
+            password: password.value,
+            phoneNumber: contact.value
+          })
+
+          console.log('Cliente cadastrado com sucesso!', response.data)
+          onReset()
+        } catch (error) {
+          console.error('Erro ao cadastrar cliente', error)
         }
       }
     }
+
+    const onReset = () => {
+      fullName.value = null
+      contact.value = null
+      email.value = null
+      password.value = null
+      confirmPassword.value = null
+      accept.value = false
+    }
+
+    return {
+      fullName,
+      contact,
+      email,
+      password,
+      confirmPassword,
+      accept,
+      onSubmit,
+      onReset
+    }
   }
-  </script>
-  
+}
+</script>
--- src/pages/lista-clientes.vue
+++ src/pages/lista-clientes.vue
@@ -1,99 +1,111 @@
 <template>
-    <q-page class="q-pa-md">
-      <div class="q-gutter-md">
-        <h1 class="text-h5">Listagem de Clientes</h1>
-        <q-card class="q-mt-md">
-          <q-card-section>
-            <!-- Listagem de clientes aqui -->
-            <div v-for="(cliente, index) in clientes" :key="index" class="flex justify-between items-center q-mb-md">
-              <q-paragraph class="q-ma-none">{{ cliente.nome }}</q-paragraph>
-              <div>
-                <q-btn @click="editarCliente(index)" color="primary" label="Editar" class="q-mr-md" />
-                <q-btn @click="removerCliente(index)" color="negative" label="Remover" />
-              </div>
+  <q-page class="q-pa-md">
+    <div class="q-gutter-md">
+      <h1 class="text-h5">Listagem de Clientes</h1>
+      <q-card class="q-mt-md">
+        <q-card-section>
+          <!-- Listagem de clientes aqui -->
+          <div v-for="(cliente, index) in clientes" :key="index" class="flex justify-between items-center q-mb-md">
+            <q-paragraph class="q-ma-none">{{ cliente.name }}</q-paragraph>
+            <div>
+              <q-btn @click="editarCliente(index)" color="primary" label="Editar" class="q-mr-md" />
+              <q-btn @click="removerCliente(index)" color="negative" label="Remover" />
             </div>
-          </q-card-section>
-        </q-card>
-      </div>
-      <!-- Modal de edição do cliente -->
-      <q-dialog v-model="mostrarModal" persistent class="larger-dialog">
-        <q-card>
-          <q-card-section>
-            <h2 class="text-h6">Editar Cliente</h2>
-            <q-input v-model="clienteEditado.nome" label="Nome" />
-            <q-input v-model="clienteEditado.senha" label="Senha" type="password" />
-            <q-input v-model="clienteEditado.cnpj" label="CNPJ" />
-            <q-input v-model="clienteEditado.email" label="E-mail" />
-            <q-input v-model="clienteEditado.telefone" label="Telefone" />
-            <!-- Adicione aqui os campos que deseja editar -->
-          </q-card-section>
-          <q-card-actions align="right">
-            <q-btn label="Cancelar" color="negative" @click="fecharModal" />
-            <q-btn label="Salvar" color="primary" @click="salvarCliente" />
-          </q-card-actions>
-        </q-card>
-      </q-dialog>
-    </q-page>
-  </template>
-  
-  <script>
-  import { defineComponent } from 'vue';
-  
-  export default defineComponent({
-    name: 'IndexPage',
-    data() {
-      return {
-        clientes: [
-          { nome: 'Jailson T.', senha: 'senha123', cnpj: '12345678901234', email: 'jailson@example.com', telefone: '123456789' },
-          { nome: 'Ricardo P.', senha: 'senha456', cnpj: '98765432109876', email: 'ricardo@example.com', telefone: '987654321' },
-          { nome: 'Cliente 3', senha: 'senha789', cnpj: '55556666777788', email: 'cliente3@example.com', telefone: '555566667' }
-          // Adicione seus clientes aqui ou carregue de uma fonte de dados
-        ],
-        mostrarModal: false,
-        clienteEditado: {
-          nome: '',
-          senha: '',
-          cnpj: '',
-          email: '',
-          telefone: ''
-          // Adicione outros campos do cliente aqui conforme necessário
-        },
-        clienteIndexEditado: null
-      };
+          </div>
+        </q-card-section>
+      </q-card>
+    </div>
+    <!-- Modal de edição do cliente -->
+    <q-dialog v-model="mostrarModal" persistent class="larger-dialog">
+      <q-card>
+        <q-card-section>
+          <h2 class="text-h6">Editar Cliente</h2>
+          <q-input v-model="clienteEditado.name" label="Nome" />
+          <q-input v-model="clienteEditado.password" label="Senha" type="password" />
+          <q-input v-model="clienteEditado.email" label="E-mail" />
+          <q-input v-model="clienteEditado.phoneNumber" label="Telefone" />
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn label="Cancelar" color="negative" @click="fecharModal" />
+          <q-btn label="Salvar" color="primary" @click="salvarCliente" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import axios from 'axios';
+
+export default defineComponent({
+  name: 'IndexPage',
+  data() {
+    return {
+      clientes: [],
+      mostrarModal: false,
+      clienteEditado: {
+        id: null,
+        name: '',
+        password: '',
+        email: '',
+        phoneNumber: ''
+      },
+      clienteIndexEditado: null
+    };
+  },
+  mounted() {
+    this.carregarClientes();
+  },
+  methods: {
+    async carregarClientes() {
+      try {
+        const response = await axios.get('https://65ff33d3df565f1a6144de0b.mockapi.io/lista-clientes');
+        this.clientes = response.data;
+      } catch (error) {
+        console.error('Erro ao carregar clientes:', error);
+      }
     },
-    methods: {
-      editarCliente(index) {
-        this.clienteEditado = { ...this.clientes[index] };
-        this.clienteIndexEditado = index;
-        this.mostrarModal = true;
-      },
-      fecharModal() {
-        this.mostrarModal = false;
-        this.clienteEditado = { nome: '', senha: '', cnpj: '', email: '', telefone: '' }; // Limpar os dados do cliente editado
-        this.clienteIndexEditado = null;
-      },
-      salvarCliente() {
-        // Aqui você pode implementar a lógica para salvar as alterações do cliente
-        console.log('Salvar alterações do cliente:', this.clienteEditado);
-        // Por exemplo, atualizar o cliente na lista:
-        if (this.clienteIndexEditado !== null) {
-          this.clientes[this.clienteIndexEditado] = { ...this.clienteEditado };
+    editarCliente(index) {
+      this.clienteEditado = { ...this.clientes[index] };
+      this.clienteIndexEditado = index;
+      this.mostrarModal = true;
+    },
+    fecharModal() {
+      this.mostrarModal = false;
+      this.clienteEditado = { id: null, name: '', password: '', email: '', phoneNumber: '' }; // Limpar os dados do cliente editado
+      this.clienteIndexEditado = null;
+    },
+    async salvarCliente() {
+      if (this.clienteIndexEditado !== null) {
+        try {
+          const cliente = this.clienteEditado;
+          await axios.put(`https://65ff33d3df565f1a6144de0b.mockapi.io/lista-clientes/${cliente.id}`, cliente);
+          this.clientes[this.clienteIndexEditado] = { ...cliente };
+        } catch (error) {
+          console.error('Erro ao salvar cliente:', error);
         }
-        this.fecharModal(); // Fechar o modal após salvar
-      },
-      removerCliente(index) {
-        // Lógica para remover cliente
-        console.log('Remover cliente:', this.clientes[index]);
+      }
+      this.fecharModal(); // Fechar o modal após salvar
+    },
+    async removerCliente(index) {
+      const clienteId = this.clientes[index].id;
+      try {
+        await axios.delete(`https://65ff33d3df565f1a6144de0b.mockapi.io/lista-clientes/${clienteId}`);
+        this.clientes.splice(index, 1); // Remove o cliente da lista localmente
+        console.log(`Cliente ${clienteId} removido com sucesso`);
+      } catch (error) {
+        console.error('Erro ao remover cliente:', error);
       }
     }
-  });
-  </script>
-  
-  <style scoped>
-  /* Estilos personalizados aqui */
-  .larger-dialog .q-card {
-    max-width: 600px; /* Ajuste conforme necessário */
-    border-radius: 0px; /* Ajuste conforme necessário */
   }
-  </style>
-  
+});
+</script>
+
+<style scoped>
+/* Estilos personalizados aqui */
+.larger-dialog .q-card {
+  max-width: 600px; /* Ajuste conforme necessário */
+  border-radius: 0px; /* Ajuste conforme necessário */
+}
+</style>
